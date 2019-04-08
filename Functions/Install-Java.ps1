b9236c3b9bf9f7e9f9278dd53927aaa7b8ce6603
Bmo1992 <brandonseahorse@gmail.com>
2019-04-08 10:01:31 -0700

    Created new script install-java

---
+++ Functions/Install-Java.ps1
@@ -0,0 +1,73 @@
+Function Install-Java
+{
+    <#
+        .SYNOPSIS
+          Function to install Java.
+
+        .DESCRIPTION
+
+        .PARAMETER InstallerPath
+          Specify a specific path to download the installer to. Default is C:\Temp.
+
+        .EXAMPLE
+
+        .EXAMPLE
+          Install-Java -InstallerPath 'C:\Users\jdoe\Downloads"
+
+          Installs Java and places the Java installer in the C:\Users\jdoe\Downloads directory for the duration of the script.
+
+        .NOTES
+          NAME    : Install-Java
+          AUTHOR  : BMO
+          EMAIL   : brandonseahorse@gmail.com
+          GITHUB  : github.com/Bmo1992
+          CREATED : April 5, 2019
+    #>
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(
+            Mandatory=$False
+        )]
+        [string]$InstallerPath
+    )
+    
+    # Set the path to the installer variable to the user specified var or set it to $env:TEMP if none specified.
+    if($InstallerPath)
+    {
+        $Path = $InstallerPath
+    }
+    else
+    {
+        $Path = $env:TEMP
+    }
+
+    # Try to download the most recent version of the Java installer 
+    Try
+    {
+        $Installer = "java_installer.exe"
+        
+        # Gets the link to the most recent version of the 64-bit version of Java for Windows
+        $link= $(Invoke-WebRequest https://www.java.com/en/download/manual.jsp).links | Where{ `
+                $_.innerHTML -like "Windows Offline (64-bit)" } | Select href,innerHTML
+
+        Invoke-WebRequest $link.href -OutFile $Path$Installer
+    }
+    Catch
+    {
+        Throw "Could download the chrome installer, please verify that the url in the script is correct."
+    }
+
+    # Try to start the chrome installer process to run silently
+    Try
+    {
+        Start-Process -FilePath $Path$Installer -Args "/silent /install" -Verb RunAs -Wait
+    }
+    Catch
+    {
+        Throw "Couldn't run the installer $Path$Installer, please confirm you have permission to this location and try again."
+    }
+
+    # Delete the chrome installer file
+    Remove-Item $Path$Installer
+}
